5b20aae616f6ffe62c580a6f698bddd01e73955b
Anus1508 <aanusutha21@gmail.com>
2026-06-04 22:40:01 +0530

    final update file

--- final-project/login.js
+++ final-project/login.js
@@ -1,94 +1,86 @@
-const form=document.querySelector('form');
-const email=document.querySelector('#emailaddress');
-const password=document.querySelector('#password');
+const form = document.querySelector("form");
+const email = document.querySelector("#emailaddress");
+const password = document.querySelector("#password");
 
-function showError(input,message){
-    const formElement=input.parentElement;
-    const p=formElement.querySelector('p');
-    const inputField=formElement.querySelector('input');
-    inputField.style.borderColor='red';
-    p.innerText=message;
-    p.style.display='block';
-    p.style.color='red';
+function showError(input, message) {
+  const formElement = input.parentElement;
+  const p = formElement.querySelector("p");
+  const inputField = formElement.querySelector("input");
+  inputField.style.borderColor = "red";
+  p.innerText = message;
+  p.style.display = "block";
+  p.style.color = "red";
 }
 
-function showSuccess(input){
-    const formElement=input.parentElement;
-    const p=formElement.querySelector('p');
-    const inputField=formElement.querySelector('input');
-    inputField.style.borderColor='green';
-    p.style.display='none';
-   
+function showSuccess(input) {
+  const formElement = input.parentElement;
+  const p = formElement.querySelector("p");
+  const inputField = formElement.querySelector("input");
+  inputField.style.borderColor = "green";
+  p.style.display = "none";
 }
 
-function getFieldName(input){
-    return input.id;
+function getFieldName(input) {
+  return input.id;
 }
-function checkLength(input,min){
-    if(input.value.length===0){
-        showError(input,`${getFieldName(input)} is required`);
-    }
-    else if(input.value.length<min){
-        showError(input,`${getFieldName(input)} must be at least ${min} characters`);
-    }
-    else{
-        showSuccess(input);
-    }
-}
-
-function checkEmail(input){
-    let email=input.value.trim();
-    if(email===''){
-        showError(input,`${getFieldName(input)} is required`);
-    }
-    else if(email!==email.toLowerCase()){
-        showError(input,`${getFieldName(input)} should be in lowercased`);
-    }
-    else{
-        showSuccess(input);
-    }
+function checkLength(input, min) {
+  if (input.value.length === 0) {
+    showError(input, `${getFieldName(input)} is required`);
+  } else if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`,
+    );
+  } else {
+    showSuccess(input);
+  }
 }
 
+function checkEmail(input) {
+  let email = input.value.trim();
+  if (email === "") {
+    showError(input, `${getFieldName(input)} is required`);
+  } else if (email !== email.toLowerCase()) {
+    showError(input, `${getFieldName(input)} should be in lowercased`);
+  } else {
+    showSuccess(input);
+  }
+}
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    
-    checkLength(password,8);
-    checkEmail(email);
+  checkLength(password, 8);
+  checkEmail(email);
+});
 
+form.addEventListener("submit", (e) => {
+  fetch(
+    "https://ekwvioyykteghvotgimj.supabase.co/auth/v1/token?grant_type=password",
+    {
+      method: "POST",
 
-})
-
-
-form.addEventListener('submit',(e)=>{fetch("https://ekwvioyykteghvotgimj.supabase.co/auth/v1/token?grant_type=password", {
-
-    method: "POST",
-
-    headers: {
+      headers: {
         "Content-Type": "application/json",
-        "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrd3Zpb3l5a3RlZ2h2b3RnaW1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NTE3NzgsImV4cCI6MjA5NDIyNzc3OH0.CYSDUnRWBmumDp9tl17XrsstX8bS9ogEIXXZOwsBFN8"
-    },
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrd3Zpb3l5a3RlZ2h2b3RnaW1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NTE3NzgsImV4cCI6MjA5NDIyNzc3OH0.CYSDUnRWBmumDp9tl17XrsstX8bS9ogEIXXZOwsBFN8",
+      },
 
-    body: JSON.stringify({
+      body: JSON.stringify({
         email: email.value,
-        password: password.value
+        password: password.value,
+      }),
+    },
+  )
+    .then((response) => response.json())
+
+    .then((data) => {
+      console.log(data);
+
+      localStorage.setItem("token1", data.access_token);
+
+      window.location.href = "createnotehomepage.html";
     })
 
-})
-.then(response => response.json())
-
-.then(data => {
-
-    console.log(data);
-
-    localStorage.setItem("token1", data.access_token);
-
-    window.location.href = "createnotehomepage.html";
-
-})
-
-.catch(error => console.log(error));
-})
-
-
+    .catch((error) => console.log(error));
+});
